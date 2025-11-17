19be06bd20b0eb9d8a445dd06536e13bf7980848
kenneth1203 <40993469+kenneth1203@users.noreply.github.com>
2025-11-17 13:41:37 +0800

    Update xueqiu.ts

--- server/sources/xueqiu.ts
+++ server/sources/xueqiu.ts
@@ -31,7 +31,45 @@ const hotstock = defineSource(async () => {
   }))
 })
 
+const hotstockhk = defineSource(async () => {
+  const url = "https://stock.xueqiu.com/v5/stock/hot_stock/list.json?size=30&_type=10&type=13"
+  const cookie = (await $fetch.raw("https://xueqiu.com/hq")).headers.getSetCookie()
+  const res: StockRes = await myFetch(url, {
+    headers: {
+      cookie: cookie.join("; "),
+    },
+  })
+  return res.data.items.filter(k => !k.ad).map(k => ({
+    id: k.code,
+    url: `https://xueqiu.com/s/${k.code}`,
+    title: k.name,
+    extra: {
+      info: `${k.percent}% ${k.exchange}`,
+    },
+  }))
+})
+
+const hotstockus = defineSource(async () => {
+  const url = "https://stock.xueqiu.com/v5/stock/hot_stock/list.json?size=30&_type=10&type=11"
+  const cookie = (await $fetch.raw("https://xueqiu.com/hq")).headers.getSetCookie()
+  const res: StockRes = await myFetch(url, {
+    headers: {
+      cookie: cookie.join("; "),
+    },
+  })
+  return res.data.items.filter(k => !k.ad).map(k => ({
+    id: k.code,
+    url: `https://xueqiu.com/s/${k.code}`,
+    title: k.name,
+    extra: {
+      info: `${k.percent}% ${k.exchange}`,
+    },
+  }))
+})
+
 export default defineSource({
   "xueqiu": hotstock,
   "xueqiu-hotstock": hotstock,
+  "xueqiu-hotstockhk": hotstock,
+  "xueqiu-hotstockus": hotstock,
 })
